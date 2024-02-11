c227c7dac9c4caaca27797b062619c1e08a11938
xmarcjordanx <mhujanen1@gmail.com>
2024-02-11 15:55:59 -0700

    API changes to express

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-var server = require("http").createServer();
+var express = require('express')
+var bodyParser = require('body-parser')
 
-server.on("request", (request, response) => {
-    var body = [];
-    request.on("data", chunk => {
-        body.push(chunk);
-    });
-    request
-        .on("end", () => {
-            let bodyString = body.concat().toString();
-            console.log(bodyString);
-            response.end(bodyString);
-        })
-        .on("error", () => {
-            response.statusCode = 400;
-            response.end();
-        });
-    response.on("error", err => {
-        console.error(err);
-    });
-});
-server.listen(process.env.PORT || 8008, () => {
-    console.log("Server listening at 8008");
-});
+var app = express()
+app.use(bodyParser.text({
+    type: function(req) {
+        return 'text';
+    }
+}));
 
-module.exports = server; // for testing
+app.post('/', (req, res)=> {
+    console.log(req.body)
+    res = res.status(200)
+    var contentType = req.get('Content-Type');
+    if (contentType) {
+        console.log("Content type:" + contentType);
+        res = res.type(contentType)
+    }
+    res.send(req.body)
+})
+
+app.listen(process.env.PORT || 8080);
+
+module.exports = app; // for testing
 
 //curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
